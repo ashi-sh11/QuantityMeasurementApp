0e6526ba6eed8e97151567ab0169b49a823d2256
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2026-03-17 11:59:38 +0530

    UseCase 4 Completed

---
+++ src/main/java/UseCase4/Length.java
@@ -0,0 +1,49 @@
+package UseCase4;
+
+public class Length {
+    private double value;
+    private LengthUnit unit;
+    public enum LengthUnit {
+        FEET(1.0),
+        INCHES(1.0 / 12.0),
+        YARDS(3.0),
+        CENTIMETERS(0.393701 / 12.0);
+        private final double conversionFactor;
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+        public double toFeet(double value) {
+            return value * conversionFactor;
+        }
+    }
+
+    public Length(double value, LengthUnit unit) {
+        if (Double.isNaN(value)) {
+            throw new IllegalArgumentException("Value must be numeric");
+        }
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null");
+        }
+        this.value = value;
+        this.unit = unit;
+    }
+    private double toBaseUnit() {
+        return unit.toFeet(value);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Length))
+            return false;
+        Length other = (Length) obj;
+        return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(toBaseUnit());
+    }
+}
