7f241d7bfc68d9e40648a7e3f24a6092242b9487
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2026-03-23 09:42:12 +0530

    UseCase 12

---
+++ src/main/java/UseCase12/LengthUnit.java
@@ -0,0 +1,28 @@
+package UseCase12;
+
+
+public enum LengthUnitUC implements IMeasurableUC12 {
+
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnitUC(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
